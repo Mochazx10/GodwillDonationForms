da39e2906ad0feba04be1b5876550cdac6ef87e5
DeShawn Wallace <deshawnwallace@DeShawns-MacBook-Air.local>
2021-11-15 14:31:46 -0500

    ID

--- goodwillDonationForms/src/main/java/com/teamB/goodwillDonationForms/models/Donation.java
+++ goodwillDonationForms/src/main/java/com/teamB/goodwillDonationForms/models/Donation.java
@@ -1,4 +1,12 @@
 package com.teamB.goodwillDonationForms.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Donation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 }
